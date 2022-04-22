02dfea3364b26262ec5a7c61355499e7207a71a5
CHRISTIAN-PC\Christian <christian.verde@live.com>
2022-04-22 10:03:43 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+'use strict';
+const path = require('path');
+
+module.exports = {
+    devtool: 'inline-source-map',
+    entry: './src/index.ts',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: 'ts-loader'
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js']
+    },
+    mode: "development",
+    devServer: {
+        open: true,
+        historyApiFallback: true
+    }
+};
